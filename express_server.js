b3b9d9608b2a621a60ab8b8ac066fde5c3b23217
matthew-charles-chan <matthew.charles.chan@gmail.com>
2020-01-20 21:48:40 +0000

    creating url index and filling out urls_index.ejs template

--- express_server.js
+++ express_server.js
@@ -9,6 +9,10 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+app.get("/urls", (req, res) => {
+  let templateVars = {urls: urlDatabase};
+  res.render("urls_index", templateVars);
+});
 app.get('/', (req, res) => {
   res.send("Hello!");
 });
@@ -19,15 +23,6 @@ app.get("/urls.json", (req,res) => {
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
-
-app.get("/set", (req, res) => {
-  const a = 1;
-  res.send(`a = ${a}`);
-});
-
-app.get("/fetch", (req, res) => {
-  res.send(`a = ${a}`)
 });
 
 
