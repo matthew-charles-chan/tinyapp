3d9c1b4adca6036c0a584e8c102043801e8d4548
matthew-charles-chan <matthew.charles.chan@gmail.com>
2020-01-21 19:59:35 +0000

    installing nodemon

--- express_server.js
+++ express_server.js
@@ -26,7 +26,6 @@ app.get("/urls/new", (req, res) => {
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render("urls_show", templateVars);
-
 });
 
 app.get("/urls", (req, res) => {
