e7dbf2e41b4a53c96585209bb9c139b6278c4b71
matthew-charles-chan <matthew.charles.chan@gmail.com>
2020-01-21 00:10:19 +0000

    moved .get(urls/new) up to stop incorrect routing

--- express_server.js
+++ express_server.js
@@ -1,19 +1,24 @@
 const express = require('express');
 const app = express();
-const PORT = 8080; 
+const PORT = 8080;
 
-app.set("view engine", "ejs")
+app.set("view engine", "ejs");
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render("urls_show", templateVars);
 
 });
+
 
 app.get("/urls", (req, res) => {
   let templateVars = {urls: urlDatabase};
@@ -34,5 +39,5 @@ app.get("/hello", (req, res) => {
 
 
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`)
+  console.log(`Example app listening on port ${PORT}!`);
 });
